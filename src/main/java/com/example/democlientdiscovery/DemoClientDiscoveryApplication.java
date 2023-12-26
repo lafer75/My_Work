e7d08b225a634f4d2fc26c20eb4397e00d3a1347
fafo8 <fafo8900@gmail.com>
2023-12-26 18:55:51 +0200

    Homework_14_Client

--- src/main/java/com/example/democlientdiscovery/DemoClientDiscoveryApplication.java
+++ src/main/java/com/example/democlientdiscovery/DemoClientDiscoveryApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -18,10 +19,40 @@ public class DemoClientDiscoveryApplication {
                 .web(WebApplicationType.SERVLET).run(args);
     }
 
+    private Map<String, String> data = new HashMap<>();
+
     @GetMapping("/services/{name}")
-    @ResponseStatus(HttpStatus.OK)
-    public String get(@PathVariable String name) {
-        return "Service name is " + name;
+    public ResponseEntity<String> get(@PathVariable String name) {
+        if (data.containsKey(name)) {
+            return ResponseEntity.ok("Service name is " + data.get(name));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
+    @PostMapping("/services")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestParam("name") String name,@RequestParam("value") String value) {
+        data.put(name, value);
+    }
+
+    @PutMapping("/services/{name}")
+    public ResponseEntity<String> update(@PathVariable String name, @RequestParam String value) {
+        if (data.containsKey(name)) {
+            data.put(name, value);
+            return ResponseEntity.ok("Service updated successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/services/{name}")
+    public ResponseEntity<String> delete(@PathVariable String name) {
+        if (data.containsKey(name)) {
+            data.remove(name);
+            return ResponseEntity.ok("Service deleted successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
